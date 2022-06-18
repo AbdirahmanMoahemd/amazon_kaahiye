42f826966f0f4b750577633560884abe783b871f
unknown <kaah6978@gmail.com>
2022-06-18 11:34:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRouter = require('./routes/admin');
+const { authRouter } = require('./routes/auth');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+
+//init server
+const app = express();
+const PORT = 5000;
+const db_url = "mongodb+srv://developerkaahiye:kaahiye1234@cluster0.vowdb.mongodb.net/flutter_ecommerce?retryWrites=true&w=majority"
+
+//middleware
+app.use(express.json())
+app.use(authRouter)
+app.use(adminRouter)
+app.use(productRouter)
+app.use(userRouter)
+
+//DB connection
+mongoose.connect(db_url).then( ()=>{
+    console.log('Connected to database')
+}).catch( e=>{
+    console.log(e)
+})
+
+
+app.listen(PORT, '0.0.0.0', ()=> {
+    console.log(`connected at port ${PORT}`);
+})
